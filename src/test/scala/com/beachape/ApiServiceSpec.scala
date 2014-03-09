972decc42beb04881c697ab91000cc68dab834b7
Lloyd <lloydmeta@gmail.com>
2014-03-09 18:08:31 +0900

    Fix trivial tests..

--- src/test/scala/com/beachape/ApiServiceSpec.scala
+++ src/test/scala/com/beachape/ApiServiceSpec.scala
@@ -5,7 +5,7 @@ import spray.testkit.Specs2RouteTest
 import spray.http._
 import StatusCodes._
 import models._
-import models.UrlScrapeJsonSupport._
+import controllers.JsonUnmarshallSupport._
 import scala.concurrent.duration._
 import com.beachape.controllers.ApiService
 
@@ -17,27 +17,21 @@ class ApiServiceSpec extends Specification with Specs2RouteTest with ApiService 
 
   "ApiService" should {
 
-    "return a greeting for GET requests to the root path" in {
-      Get() ~> apiRoutes ~> check {
-        responseAs[String] must contain("Say hello")
-      }
-    }
-
     "leave GET requests to other paths unhandled" in {
       Get("/kermit") ~> apiRoutes ~> check {
         handled must beFalse
       }
     }
 
-    "return a MethodNotAllowed error for PUT requests to the root path" in {
-      Put() ~> sealRoute(apiRoutes) ~> check {
+    "return a MethodNotAllowed error for PUT requests to the scrape path" in {
+      Put("/api/scrape_url") ~> sealRoute(apiRoutes) ~> check {
         status === MethodNotAllowed
-        responseAs[String] === "HTTP method not allowed, supported methods: GET"
+        responseAs[String] === "HTTP method not allowed, supported methods: POST"
       }
     }
 
     "return an OK response for POST requests to scrape_url" in {
-      Post("/scrape_url", UrlScrape("http://www.google.com")) ~> apiRoutes ~> check {
+      Post("/api/scrape_url", UrlScrape("http://www.google.com")) ~> apiRoutes ~> check {
         status === OK
       }
     }
